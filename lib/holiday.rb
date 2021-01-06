1d241eabc265ccb8cdb2fc96865b74627931268d
Kristina Frederick <kristinafrederick@icloud.com>
2021-01-06 20:31:47 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,5 +65,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-  
+  bbq_holiday = []
+  holiday_hash.each do |season, holidays|
+    
 end
