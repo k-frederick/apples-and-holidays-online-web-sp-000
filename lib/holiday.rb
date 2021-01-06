31463fe39e3f09768dd708a1ea655f329831c468
Kristina Frederick <kristinafrederick@icloud.com>
2021-01-06 20:25:14 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  
+  holiday_hash.each do |seasons, holidays|
+    puts {season.capitalize}
+    holidays.each do |holiday, supplies|
+      puts "#{holiday.to.split(' ').map {|w| w.capitalize}.join(' ') }: #{supplies.join(", ")}"
 end
 
 def all_holidays_with_bbq(holiday_hash)
